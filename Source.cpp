a950e24b4619ff2025ec20fc7b0d4f494d6034df
Nguyen Duy Thuc <duythuc08tt@gmail.com>
2025-11-04 08:04:02 +0700

    NguyenDuyThuc_Tich

--- Source.cpp
+++ Source.cpp
@@ -3,12 +3,16 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
+int tich(int a, int b){
+	return a*b;
+}
 float thuong(int a, int b);
 int main()
-{
+{	
+	
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong =";
+	cout<<"Tich = " << tich(2*3);
 	system("pause");
 	return 0;
 }
