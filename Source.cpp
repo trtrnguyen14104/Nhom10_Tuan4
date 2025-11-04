720fe7e9ff737a7415121322e99f613017834875
Nguyen Duy Thuc <duythuc08tt@gmail.com>
2025-11-04 08:12:28 +0700

    Revert "NguyenDuyThuc_Tich"
    This reverts commit a950e24b4619ff2025ec20fc7b0d4f494d6034df.

--- Source.cpp
+++ Source.cpp
@@ -3,16 +3,12 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b){
-	return a*b;
-}
+int tich(int a, int b);
 float thuong(int a, int b);
 int main()
-{	
-	
+{
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong =";
-	cout<<"Tich = " << tich(2*3);
 	system("pause");
 	return 0;
 }
