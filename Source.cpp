fd029cb21662221cbf44dcfd083732d2d8087592
Tran Trung Nguyen <trtrnguyen14104@gmail.com>
2025-11-04 08:23:00 +0700

    Them ham tinh luy thua

--- Source.cpp
+++ Source.cpp
@@ -7,6 +7,16 @@ int tong(int a, int b){
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+
+int tinhLuyThua(int a, int n) {
+	int kq = a;
+	if (n == 0) kq = 1; 
+	for(int i = 1; i < n; i++) {
+		kq *= a;
+	}
+	return kq;
+ };
+
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
