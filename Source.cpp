6b7f506b8a63e54924c6e2c48a03df5fb949b694
MinhTruong204 <truongocminh204@gmail.com>
2025-11-04 08:05:57 +0700

    minh commit

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,16 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b) {
+	return a - b;
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong =";
+	cout<<"Hieu =" << hieu(3,2);
 	system("pause");
 	return 0;
 }
