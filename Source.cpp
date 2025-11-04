511dd45babcadeba873a8c697f23bef13954d480
Tran Trung Nguyen <trtrnguyen14104@gmail.com>
2025-11-04 08:04:51 +0700

    them ham tong

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b){
+	return a + b;
+};
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Tong =";
+	cout<<"Tong = "tong(2,3);
 	system("pause");
 	return 0;
 }
